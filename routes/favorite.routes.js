910857190d2611d70fe27353c376cc7af9a55ffc
ddalynng <d.fangloy@gmail.com>
2025-01-20 22:49:57 +0100

    user favorite route

--- routes/favorite.routes.js
+++ routes/favorite.routes.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const Favorite = require("../models/Favorite.model");
 
 //Get all favorite
-
 router.get("/all-favorites", async (req, res) => {
   try {
     const allFavorites = await Favorite.find();
@@ -13,3 +12,20 @@ router.get("/all-favorites", async (req, res) => {
     res.status(500).json({ message: "Failed to fetch all favorites" });
   }
 });
+
+//Get user's favorite
+
+router.get("/your-favorite/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const userFavorite = await ProgressEvent.find({ user: userId }).populate(
+      "user"
+    );
+    res.status(200).json(userFavorite);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to fetch user's favorite" });
+  }
+});
+
+module.exports = router;
